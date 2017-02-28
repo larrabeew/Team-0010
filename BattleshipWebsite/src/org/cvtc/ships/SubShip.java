953cc9cd4b0b6f30cd3099e65b1adaef07e37c8c
Wesley Larrabee <WLarrabee@RiceLake.com>
2017-02-28 10:50:50 -0600

    Commit project

---
+++ BattleshipWebsite/src/org/cvtc/ships/SubShip.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.cvtc.ships;
+
+/**
+ * @author wlarrabee
+ *
+ */
+public class SubShip extends Ship {
+	
+	public SubShip(String direction, int startX, int startY) {
+		super(3, direction, startX, startY);
+	}
+
+}
