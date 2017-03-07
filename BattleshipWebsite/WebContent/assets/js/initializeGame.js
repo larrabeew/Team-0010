f86e525e32bd0d285bdf04246243d8f6e3bed9cc
woodfobm <bwoodford8@student.cvtc.edu>
2017-03-06 19:58:48 -0600

    Made ship images snappable to grid.

--- BattleshipWebsite/WebContent/assets/js/initializeGame.js
+++ BattleshipWebsite/WebContent/assets/js/initializeGame.js
@@ -1,5 +1,39 @@
 $(function() {
 	
+	var arrayBoard = [];
+	
+	var ships = [];
+	
+	//ships.push({name: , length:});
+	
+	var selectedShip = {
+		name: "",
+			
+		get className() {
+			return this._name
+		},
+		set className(name) {
+			this._name = name;
+		}
+	}
+	
+	var selectedDirection = {
+		
+	    // UP and DOWN will be the
+		// only possible values
+		dir: "UP",
+			
+		get direction() {
+			return this._dir
+		},
+		set direction(dir) {
+			this._dir = dir;
+		}
+	
+	}
+	
+	var $shipImages = $("div#shipDock > img");
+	
 	// Clicks on enemy board
 	$('table#enemyBoard > tbody > tr > td').click(function() {
 		
@@ -9,14 +43,53 @@ $(function() {
 		
 	});
 	
-	// Clicks on ship images
-	//$('').click() {
+	// Change Ship Orientation
+	$shipImages.dblclick(function() {
 		
-	//}
+		if(selectedDirection.dir == "DOWN") {
+			
+			$(this).css({
+				'transform':          'rotate(0deg)',
+				'-ms-transform':      'rotate(0deg)',
+				'-moz-transform':     'rotate(0deg)',
+				'-webkit-transform':  'rotate(0deg)',
+				'-o-transform':       'rotate(0deg)'
+			});
+			
+			selectedDirection.dir = "UP";
+		} else {
+			
+			$(this).css({
+				'transform':          'rotate(90deg)',
+				'-ms-transform':      'rotate(90deg)',
+				'-moz-transform':     'rotate(90deg)',
+				'-webkit-transform':  'rotate(90deg)',
+				'-o-transform':       'rotate(90deg)'
+			});
+			
+			selectedDirection.dir = "DOWN";
+			
+		}
+		
+	});
 	
-	// Clicks on user board
-	//$('').click() {
-		
-	//}
+	// Dragging on ship images
+	$shipImages.draggable({
+		snap: 'table#userBoard > tbody > tr > td',
+		snapTolerance: 35,
+		snapMode: "both",
+		opacity: 0.35,
+		cursorAt: { left: 14.5, top: 3},
+		drag: function() {
+			selectedShip.name = $(this).attr("id");
+		}
+	});
+	
+	$("table#userBoard > tbody > tr > td").droppable({
+		accept: "img",
+		drop: function() {
+			$(this).addClass(selectedShip.name);
+		}
+	});
 	
 });
